6dfa37ccc24d94f24dde09046d4c4002624e894d
JMatharu <jagdeepmatharu88@gmail.com>
2016-06-05 15:24:06 -0400

    Adding AppiumFieldDecorator

--- src/main/helper/common/WPAppiumPageTemplate.java
+++ src/main/helper/common/WPAppiumPageTemplate.java
@@ -3,6 +3,8 @@ package common;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
 /**
  * Page Object Template - This abstract class should be extended by all Page
  * Object Classes in Project for Initializing Element
@@ -16,6 +18,6 @@ public abstract class WPAppiumPageTemplate {
 	public WPAppiumPageTemplate(WebDriver driver) {
 		this.driver = driver;
 		// Initialise Elements
-		PageFactory.initElements(driver, this);
+		PageFactory.initElements(new AppiumFieldDecorator(driver), this);
 	}
 }
