e4e1e7fd78666433644a6198a553e4d9bc1c7c8b
JMatharu <jagdeepmatharu88@gmail.com>
2016-06-05 15:20:55 -0400

    Closing #3 Login helper page test

---
+++ src/main/helper/loginPage/pageTest/LoginHelperPageTest.java
@@ -0,0 +1,41 @@
+package loginPage.pageTest;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import com.wp.automation.page.MainActivityPage;
+
+import common.WPAppiumTemplate;
+import common.data.ReadFromCSV;
+import loginPage.page.LoginHelperPage;
+
+public class LoginHelperPageTest extends WPAppiumTemplate {
+	LoginHelperPage loginHelperPageObject = new LoginHelperPage(driver);
+	ReadFromCSV readFromCSVObject = new ReadFromCSV();
+	MainActivityPage mainActivityObj = new MainActivityPage(driver);
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	@Test(enabled = true)
+	public void clickSignIn() throws InterruptedException {
+		loginHelperPageObject.enterUserName(readFromCSVObject.getLoginUserName());
+		loginHelperPageObject.enterPassword(readFromCSVObject.getLoginPassword());
+		loginHelperPageObject.clickSignIn();
+		Assert.assertEquals(getCurrentActivity(), ".ui.main.WPMainActivity");
+	}
+
+	@Test(enabled = false)
+	public void enterPassword() {
+		loginHelperPageObject.enterPassword(readFromCSVObject.getLoginPassword());
+		System.out.println("--- " + loginHelperPageObject.getPassword());
+	}
+
+	@Test(enabled = false)
+	public void enterUserName() {
+		loginHelperPageObject.enterUserName(readFromCSVObject.getLoginUserName());
+	}
+}
